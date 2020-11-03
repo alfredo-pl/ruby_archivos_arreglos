b6e4f95a4413a036e87373c799e05b148f80afd3
alfredopl <aalfredoplz@gmail.com>
2020-11-03 13:43:50 -0300

    output print eje

--- grafico.rb
+++ grafico.rb
@@ -1,11 +1,18 @@
 
 def chart(array)
-    
+    mayor= 0
     array.size.times do |i|
+        if mayor < array[i].to_i
+            mayor = array[i].to_i
+        end
         asteriscos = '*' * array[i].to_i * 2
         print "|"+ asteriscos+"\n"
     end
-  
+   eje = '-' * mayor * 2
+   print ">"+eje+"\n"
+        mayor.times do |i|
+            print "#{i+1} "
+        end
 end
 
 chart(ARGV)
